f536838034d15559ee94a550161b8d0f579cab37
Matthew Hur <mhur0724@gmail.com>
2025-05-22 12:22:05 -0500

    testing again

--- backend/routes/playersRouter.js
+++ backend/routes/playersRouter.js
@@ -1,7 +1,16 @@
+// const { Router } = require("express");
+// const { getPlayers} = require("../controllers/playersControllers");
+// const playersRouter = Router();
+
+// playersRouter.get("/", getPlayers);
+
+// module.exports = playersRouter;
 const { Router } = require("express");
-const { getPlayers} = require("../controllers/playersControllers");
 const playersRouter = Router();
 
-playersRouter.get("/", getPlayers);
+playersRouter.get("/", (req, res) => {
+  console.log("✅ /players route hit");
+  res.json([{ id: 1, name: "Test Player" }]);
+});
 
 module.exports = playersRouter;
